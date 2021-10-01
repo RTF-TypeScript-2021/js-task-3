aae17c960ab4852b751baa150f937e711a32aa97
makar <misha.torno@gmail.com>
2021-10-01 21:53:39 +0500

    решил первый таск

--- topic-3/task-1/index.js
+++ topic-3/task-1/index.js
@@ -1,22 +1,41 @@
 "use strict";
+
 /** Задача 1 - Класс Time
-Требуется написать класс времени - Time, который содержит:
-	1.1. Поле с часами — hours (number)
-	1.2. Поле с минутами — minutes (number)
-	1.3. Прототип класса должен содержать метод сравнения isEarlier,
-	который принимает объект класса Time и возвращает true,
-	если переденное значение времени находится позже того,
-	которое содержится в экземпляре объекта, у которого вызван метод.
-	1.4. Прототип класса должен содержать метод сравнения isLater,
-	который принимает объект класса Time и возвращает true,
-	если переденное значение времени находится раньше того,
-	которое содержится в экземпляре объекта, у которого вызван метод.
-@constructor
-@this {Time}
-@param {number} hours - Час
-@param {number} minutes - Минуты
+ Требуется написать класс времени - Time, который содержит:
+ 1.1. Поле с часами — hours (number)
+ 1.2. Поле с минутами — minutes (number)
+ 1.3. Прототип класса должен содержать метод сравнения isEarlier,
+ который принимает объект класса Time и возвращает true,
+ если переденное значение времени находится позже того,
+ которое содержится в экземпляре объекта, у которого вызван метод.
+ 1.4. Прототип класса должен содержать метод сравнения isLater,
+ который принимает объект класса Time и возвращает true,
+ если переденное значение времени находится раньше того,
+ которое содержится в экземпляре объекта, у которого вызван метод.
+ @constructor
+ @this {Time}
+ @param {number} hours - Час
+ @param {number} minutes - Минуты
  */
+
 function Time(hours, minutes) {
+    if (!Number.isInteger(hours) || !Number.isInteger(minutes) || hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+        throw new Error;
+    }
+    this.hours = hours;
+    this.minutes = minutes;
+}
+
+Time.prototype.isEarlier = function (time) {
+    if (!(time instanceof Time)) {
+        throw new Error;
+    }
+
+    return time.hours > this.hours || time.hours === this.hours && time.minutes > this.minutes
+}
+
+Time.prototype.isLater = function (time) {
+    return !this.isEarlier(time);
 }
 
 module.exports.Time = Time;

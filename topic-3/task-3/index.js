78ff379b32900f8576313e84a31c7b53b1a9d464
makar <misha.torno@gmail.com>
2021-10-01 23:22:21 +0500

    решил третий таск

--- topic-3/task-3/index.js
+++ topic-3/task-3/index.js
@@ -1,18 +1,32 @@
 /** Задача 3 - Класс Vacation
-Требуется написать класс отпуска - Vacation, который содержит:
-	3.1. Дата начала (объект класса Date)
-	3.2. Дата окончания (объект класса Time)
-	3.3. Прототип класса должен содержать метод isDateInVacation, принимающий один аргумент — дату.
-	Должен возвращать true, если переданная дата, входит в промежуток отпуска.
-	3.4. Дата окончания отпуска должна быть позже даты начала
-@constructor
-@this {Vacation}
-@param {Date} vacationStartDate - Дата начала отпуска
-@param {Date} vacationEndDate - Время конца отпуска
+ Требуется написать класс отпуска - Vacation, который содержит:
+ 3.1. Дата начала (объект класса Date)
+ 3.2. Дата окончания (объект класса Time)
+ 3.3. Прототип класса должен содержать метод isDateInVacation, принимающий один аргумент — дату.
+ Должен возвращать true, если переданная дата, входит в промежуток отпуска.
+ 3.4. Дата окончания отпуска должна быть позже даты начала
+ @constructor
+ @this {Vacation}
+ @param {Date} vacationStartDate - Дата начала отпуска
+ @param {Date} vacationEndDate - Время конца отпуска
  */
 
 function Vacation(vacationStartDate, vacationEndDate) {
+    if (!(vacationStartDate instanceof Date)
+        || !(vacationEndDate instanceof Date)
+        || vacationEndDate.getTime() <= vacationStartDate.getTime()) {
+        throw new Error();
+    }
+    this.vacationStartDate = vacationStartDate;
+    this.vacationEndDate = vacationEndDate;
+}
 
+Vacation.prototype.isDateInVacation = function (date) {
+    if (!(date instanceof Date)) {
+        throw new Error();
+    }
+
+    return this.vacationStartDate.getTime() <= date.getTime() && this.vacationEndDate >= date.getTime();
 }
 
 module.exports.Vacation = Vacation;

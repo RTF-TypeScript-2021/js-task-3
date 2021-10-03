555e08d60380db9e389a71f73b4d1e0a8167cbae
alexeyvolhin <82944068+alexeyvolhin@users.noreply.github.com>
2021-10-03 22:45:04 +0500

    Task1

--- topic-3/task-1/index.js
+++ topic-3/task-1/index.js
@@ -17,6 +17,22 @@
 @param {number} minutes - Минуты
  */
 function Time(hours, minutes) {
+    /* проверка входных значений на адекатность */
+    if (typeof(hours,minutes) !=="number" || minutes < 0 || hours < 0 || hours > 23 || minutes > 59) {
+        throw new Error("Time data must be number that more than 0");
+    }
+    /* задание контекста для аргументов hours и minutes */
+    this.hours = hours;
+    this.minutes = minutes;
+}
+/*Переводим в Си, делаем проверку */
+Time.prototype.isEarlier = function(Time) { 
+    return Time.hours * 3600 + Time.minutes * 60 > this.hours * 3600 + this.minutes * 60
+}
+/* аналогично предыдущему методу только later */
+Time.prototype.isLater = function(Time) {
+    return Time.hours * 3600 + Time.minutes * 60 < this.hours * 3600 + this.minutes * 60
+
 }
 
 module.exports.Time = Time;

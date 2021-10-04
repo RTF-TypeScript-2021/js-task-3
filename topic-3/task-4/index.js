0a7841e1ea13b47c76016099c40ca2af5caa9b9f
Sychqh <sychqh@gmail.com>
2021-10-04 16:19:32 +0500

    Сделал четвертое задание

--- topic-3/task-4/index.js
+++ topic-3/task-4/index.js
@@ -7,8 +7,19 @@
 @param {Array<Meeting>} meetings - Массив встреч
 @param {Array<Vacation>} vacations - Массив отпусков
  */
+const {Meeting} = require("../task-2");
+const {Vacation} = require("../task-3");
 
-function Organaizer(meetings = [], vacations = []) { 
-};
+function Organaizer(meetings = [], vacations = []) {
+    if (!(Array.isArray(meetings)) || meetings.some(meeting => !(meeting instanceof Meeting))) {
+        throw new Error("Meetings variable must be an array of Meeting");
+    }
+    if (!(Array.isArray(vacations)) || vacations.some(vacation => !(vacation instanceof Vacation))) {
+        throw new Error("Vacations variable must be an array of Vacation");
+    }
+	
+    this.meetings = meetings;
+    this.vacations = vacations;
+}
 
 module.exports.Organaizer = Organaizer;

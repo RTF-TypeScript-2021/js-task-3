b8be21482427e8d1fdf6b9032fc353d1e4e75be2
Sychqh <sychqh@gmail.com>
2021-10-04 15:38:25 +0500

    Сделал второе задание

--- topic-3/task-2/index.js
+++ topic-3/task-2/index.js
@@ -7,16 +7,38 @@
 		Начало временного промежутка — объект класса Time
 		Конец временного промежутка — объект класса Time
 		Должен возвращать true, если встреча, у которой был вызван метод,
-		пересекает переданный временной промежутук
+		пересекает переданный временной промежуток
 	2.5. Время начала встречи должно быть больше времени конца
-	2.6. Встреча может быть назначана только в промежутке между 08:00 до 19:00
+	2.6. Встреча может быть назначена только в промежутке между 08:00 до 19:00
 @constructor
 @this {Meeting}
 @param {Date} meetingDate - Дата встречи
 @param {Time} startTime - Время начала встречи
 @param {Time} endTime - Время конца встречи
  */
+const { Time } = require("../task-1");
+
 function Meeting(meetingDate, startTime, endTime) {
+    if (!(meetingDate instanceof Date)) {
+        throw new Error("Meeting Date variable must be an instance of Date");
+    }
+    if (!(startTime instanceof Time)) {
+        throw new Error("Start time variable must be an instance of Time");
+    }
+    if (!(endTime instanceof Time)) {
+        throw new Error("End time variable must be an instance of Time");
+    }
+    if (startTime.hours < 8 || endTime > 19) {
+        throw new Error("The meeting must happen between 8:00 and 19:00");
+    }
+	
+    this.meetingDate = meetingDate;
+    this.startTime = startTime;
+    this.endTime = endTime;
+}
+
+Meeting.prototype.isMeetingInTimeRange = function (startTime, endTime) {
+    return this.endTime.isLater(startTime) && this.startTime.isEarlier(endTime)
 }
 
 module.exports.Meeting = Meeting;

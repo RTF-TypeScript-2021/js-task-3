1acfd5c8a1b400c23b576acdfe2b9f1cf38cb8a8
HappyRoma <krivonogov.roma@mail.ru>
2021-10-04 18:45:48 +0500

    All tasks

--- topic-3/task-1/index.js
+++ topic-3/task-1/index.js
@@ -1,22 +1,40 @@
 "use strict";
+
 /** Задача 1 - Класс Time
-Требуется написать класс времени - Time, который содержит:
-	1.1. Поле с часами — hours (number)
-	1.2. Поле с минутами — minutes (number)
-	1.3. Прототип класса должен содержать метод сравнения isEarlier,
-	который принимает объект класса Time и возвращает true,
-	если переденное значение времени находится позже того,
-	которое содержится в экземпляре объекта, у которого вызван метод.
-	1.4. Прототип класса должен содержать метод сравнения isLater,
-	который принимает объект класса Time и возвращает true,
-	если переденное значение времени находится раньше того,
-	которое содержится в экземпляре объекта, у которого вызван метод.
-@constructor
-@this {Time}
-@param {number} hours - Час
-@param {number} minutes - Минуты
+ Требуется написать класс времени - Time, который содержит:
+ 1.1. Поле с часами — hours (number)
+ 1.2. Поле с минутами — minutes (number)
+ 1.3. Прототип класса должен содержать метод сравнения isEarlier,
+ который принимает объект класса Time и возвращает true,
+ если переденное значение времени находится позже того,
+ которое содержится в экземпляре объекта, у которого вызван метод.
+ 1.4. Прототип класса должен содержать метод сравнения isLater,
+ который принимает объект класса Time и возвращает true,
+ если переденное значение времени находится раньше того,
+ которое содержится в экземпляре объекта, у которого вызван метод.
+ @constructor
+ @this {Time}
+ @param {number} hours - Час
+ @param {number} minutes - Минуты
  */
 function Time(hours, minutes) {
+    if (hours < 0 || hours > 23) {
+        throw new Error("Incorrect hours");
+    }
+    if (minutes < 0 || minutes > 59) {
+        throw new Error("Incorrect minutes");
+    }
+
+    this.hours = hours;
+    this.minutes = minutes;
+
+    this.isEarlier = function (time) {
+        return this.hours * 60 + this.minutes < time.hours * 60 + time.minutes;
+    }
+
+    this.isLater = function isLater(time) {
+        return this.hours * 60 + this.minutes > time.hours * 60 + time.minutes;
+    }
 }
 
 module.exports.Time = Time;
--- topic-3/task-3/index.js
+++ topic-3/task-3/index.js
@@ -1,18 +1,29 @@
 /** Задача 3 - Класс Vacation
-Требуется написать класс отпуска - Vacation, который содержит:
-	3.1. Дата начала (объект класса Date)
-	3.2. Дата окончания (объект класса Time)
-	3.3. Прототип класса должен содержать метод isDateInVacation, принимающий один аргумент — дату.
-	Должен возвращать true, если переданная дата, входит в промежуток отпуска.
-	3.4. Дата окончания отпуска должна быть позже даты начала
-@constructor
-@this {Vacation}
-@param {Date} vacationStartDate - Дата начала отпуска
-@param {Date} vacationEndDate - Время конца отпуска
+ Требуется написать класс отпуска - Vacation, который содержит:
+ 3.1. Дата начала (объект класса Date)
+ 3.2. Дата окончания (объект класса Time)
+ 3.3. Прототип класса должен содержать метод isDateInVacation, принимающий один аргумент — дату.
+ Должен возвращать true, если переданная дата, входит в промежуток отпуска.
+ 3.4. Дата окончания отпуска должна быть позже даты начала
+ @constructor
+ @this {Vacation}
+ @param {Date} vacationStartDate - Дата начала отпуска
+ @param {Date} vacationEndDate - Время конца отпуска
  */
 
 function Vacation(vacationStartDate, vacationEndDate) {
+    if (vacationEndDate <= vacationStartDate) {
+        throw new Error("Отпуск не может закончиться раньше, чем он начался");
+    }
+    if (!Date.prototype.isPrototypeOf(vacationStartDate) || !Date.prototype.isPrototypeOf(vacationEndDate)) {
+        throw new Error("Некорректные входные данные");
+    }
+    this.vacationStartDate = vacationStartDate;
+    this.vacationEndDate = vacationEndDate;
 
+    this.isDateInVacation = function (date) {
+        return date >= this.vacationStartDate && date <= this.vacationEndDate;
+    }
 }
 
 module.exports.Vacation = Vacation;

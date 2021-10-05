a6ad052ca2795225fb3ad2c48377a896c23aefe8
Ninel <inel2008@bk.ru>
2021-10-06 01:36:26 +0500

    Complete task-4

--- topic-3/task-4/index.js
+++ topic-3/task-4/index.js
@@ -8,7 +8,21 @@
 @param {Array<Vacation>} vacations - Массив отпусков
  */
 
-function Organaizer(meetings = [], vacations = []) { 
+function Organaizer(meetings = [], vacations = []) {
+    this.meetings = meetings;
+    this.vacations = vacations;
+
+    if(!Array.isArray(meetings) || !Array.isArray(vacations)) {
+        throw Error("Arguments are not arrays");
+    }
+
+    if (meetings.some(date => !(date instanceof Meeting))) {
+        throw Error("First argument must be instance of Meeting object");
+    }
+
+    if (vacations.some(date => !(date instanceof Vacation))) {
+        throw Error("Second argument must be instance of Vacation object");
+    }
 };
 
 module.exports.Organaizer = Organaizer;
